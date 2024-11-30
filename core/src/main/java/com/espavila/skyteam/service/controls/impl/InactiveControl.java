026556ade9e894cd1e71875aa59bf929d6b51847
Marina Vereshchagina <marinafjell@gmail.com>
2024-11-30 21:41:11 +0100

    Initial commit

---
+++ core/src/main/java/com/espavila/skyteam/service/controls/impl/InactiveControl.java
@@ -0,0 +1,53 @@
+package com.espavila.skyteam.service.controls.impl;
+
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.espavila.skyteam.configuration.preferences.ControlsData;
+import com.espavila.skyteam.service.controls.Control;
+import com.espavila.skyteam.service.controls.ControlListener;
+import com.espavila.skyteam.service.controls.ControlType;
+
+/** Mock-up controls representing an inactive player. */
+public class InactiveControl implements Control {
+    @Override
+    public boolean isActive() {
+        return false;
+    }
+
+    @Override
+    public ControlType getType() {
+        return ControlType.INACTIVE;
+    }
+
+    @Override
+    public void attachInputListener(final InputMultiplexer inputMultiplexer) {
+    }
+
+    @Override
+    public void update(final Viewport gameViewport, final float gameX, final float gameY) {
+    }
+
+    @Override
+    public Vector2 getMovementDirection() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setControlListener(final ControlListener listener) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ControlsData toData() {
+        return new ControlsData(getType());
+    }
+
+    @Override
+    public void copy(final ControlsData data) {
+    }
+
+    @Override
+    public void reset() {
+    }
+}
