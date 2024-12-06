1775bb40b4fca899209b98ed6d0af7f7770191aa
Antonio Melero Parra <ameleroparra@hof-university.de>
2024-12-06 14:16:00 +0100

    Start Axis class

--- core/src/main/java/com/espabila/skyteam/model/Axis.java
+++ core/src/main/java/com/espabila/skyteam/model/Axis.java
@@ -1,4 +1,12 @@
 package com.espabila.skyteam.model;
 
 public class Axis {
+     private int[] position;
+     private int currentIndexPosition;
+
+     public Axis() {
+         this.position = new int[] {-2, -1, 0, 1, 2};
+         this.currentIndexPosition = 2;
+     }
+
 }
